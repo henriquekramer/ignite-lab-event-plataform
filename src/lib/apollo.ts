7b0cf2ecc5c23a3f97ed71a6e385abbfc8032386
Henrique Kramer <henriquemkramer@gmail.com>
2022-06-25 14:31:08 -0300

    Implementação do sidebar

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: '',
+  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4tu7dnj08qz01uig8gj3sko/master',
   cache: new InMemoryCache()
 })
