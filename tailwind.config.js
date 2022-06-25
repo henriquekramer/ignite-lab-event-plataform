b7f8a17ae7686fc5a580084c0906a716053866cd
Henrique Kramer <henriquemkramer@gmail.com>
2022-06-24 23:38:09 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ //passamos quais arquivos vamos usar o tailwind
+    './src/**/*.tsx'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
