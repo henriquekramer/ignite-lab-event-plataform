9ea3c2d19467ca45a274c76ee76b5fc5faaceb3f
Henrique Kramer <henriquemkramer@gmail.com>
2022-06-26 15:06:25 -0300

    Imnplementação da Inscrição via GraphQL

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4tu7dnj08qz01uig8gj3sko/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACESS_TOKEN}`
+  },
   cache: new InMemoryCache()
 })
